e1019c9127bd9f735ff965ee350a464186b674ae
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2025-01-23 14:01:15 +0530

    Done with the basic design and functionalities for the Frogger game

---
+++ JavaScript/Frogger/index.js
@@ -0,0 +1,30 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+let currentIndex = 76
+const width = 9
+
+function moveFrog(e) {
+    
+    switch(e.key) {
+        case 'ArrowLeft':
+            if(currentIndex % width !== 0) currentIndex -=1
+            break
+        case 'ArrowUp':
+            if(currentIndex - width >= 0) currentIndex -= width
+            break
+        case 'ArrowRight':
+            if(currentIndex % width < width -1) currentIndex +=1
+            break
+        case 'ArrowDown':
+            if(currentIndex + width < width * width) currentIndex += width
+            break
+    }
+
+    squares[currentIndex].classList.add('frog')
+
+}
+
+document.addEventListener('keyup', moveFrog)
